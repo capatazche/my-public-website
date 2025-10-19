daffa1bdfaf8883462f72ddab36108ec58df6111
Capataz Che <38944094+capatazche@users.noreply.github.com>
2025-10-19 08:32:16 -0500

    Amplify Gen 2 Change (`iam` to `identityPool`)

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -26,6 +26,6 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
     schema,
     authorizationModes: {
-        defaultAuthorizationMode: 'iam',
+        defaultAuthorizationMode: 'identityPool',
     },
 });
